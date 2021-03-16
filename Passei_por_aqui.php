ba1bd4ec87581e3e191b9a33297b49120e02aea7
joaosesconetto <78798315+joaosesconetto@users.noreply.github.com>
2021-03-16 11:01:10 -0300

    "criei metodos de lista"

---
+++ Passei_por_aqui.php
@@ -0,0 +1,10 @@
+<?php
+
+class Passei_por_aqui {
+
+	public function echoMsg($id_msg){
+		// echo $id_msg;
+	}
+}
+		
+?>
--- index.php
+++ index.php
@@ -2,18 +2,39 @@
 // o PHP irá verificar se o arquivo já foi incluído e, em caso afirmativo, não o incluirá (solicitará) novamente.
 
 // abaixo é meu teste, na passagem do programa por aqui
-echo "Passo 1.1 no index.php - Verifica se o arquivo de configuração esta carregado e carrega se não estiver ==>"."<br>"."<br>";
+// echo "Passo 1.1 no index.php - Verifica se o arquivo de configuração esta carregado e carrega se não estiver ==>"."<br>"."<br>";
+
+
 require_once("config.php");
 // Aqui eu atribui a uma nova váriavel "$sql" a minha classe Sql() criada no arquivo sql.php, por que não posso fazer ações diretamente naquela classe Sql(), por isso tenho que fazer por referencia, criando esta referencia como abaixo;
 
-$sql = new Sql();
+require_once("Passei_por_aqui.php");
+$texto = "Passo 1.1 no index.php - Verifica se o arquivo de configuração esta carregado e carrega se não estiver ==>"."<br>"."<br>";
+$msg = new Passei_por_aqui();
+$msg->echoMsg($texto);
 
-// abaixo é meu teste, na passagem do programa por aqui
-echo "Passo 4.1 função Sql até este ponto esta vazia ". is_string($sql) . "<br>";
-echo "Passo 4.2 Monta o SELECT e chama a função select: 'SELECT * FROM tb_usuarios' "."<br>"."<br>";	
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+// carreta um usuário especifico
+// $root = new Usuario();
+// $root->loadbyId(1);
+// echo $root;
 
+// carrega uma lista de usuários
+// Abaixo escrevemos um metódo estatico. Portanto não utilizamos o this. Funciona da forma statica ou como esta nos segundos comandos abaixo.
+// $lista = Usuario::getList();
+// echo json_encode($lista);
 
-echo json_encode($usuarios);
+// carrega toda a tabela de usuários
+// $usuarios = $sql->select("SELECT * FROM tb_usuarios");
+// echo json_encode($usuarios);
 
+// carrega uma lista de usuários buscando pelo login do usuário. Utilizando a condição like
+// $search = Usuario::search("jo");
+// echo json_encode($search);
+
+// carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+// utilizando o metódo login;
+$usuario->login("joao", "999");
+echo $usuario;
 ?>
+
