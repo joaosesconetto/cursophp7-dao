14cff40ced453ceb3f631cf2ddaa28011d86bd77
joaosesconetto <78798315+joaosesconetto@users.noreply.github.com>
2021-03-16 11:00:28 -0300

    "criei metódos de lista"

--- Usuario.php
+++ Usuario.php
@@ -43,6 +43,7 @@ class Usuario {
 		$this->dtcadastro = $value;
 	}
 
+// A função loadByid foi feita para carregar um usuário só por vez
 	public function loadByid($id){
 
 		$sql = new Sql();
@@ -53,7 +54,7 @@ class Usuario {
 		if (count($results) > 0){
 
 			$row = $results[0];
-
+						
 			$this->setIdusuario($row['idusuario']);
 			$this->setDeslogin($row['deslogin']);
 			$this->setDessenha($row['dessenha']);
@@ -62,7 +63,46 @@ class Usuario {
 		}
 	}
 
+		public static function getList(){
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY idusuario;");
+		
+	}
+
+	public static function search($login){
+
+		$sql = new Sql();
+// Da forma como esta sendo feito o cmd sql abaixo, evita o haqueamento por conta de algum malfeitor infiar um cmd dentro da nossa linha de sql utilizando o caracter aspas.
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%" ));
+	}
+
+// Quando a função não for static como a de baixo, devemos utilizar a classe como por exemplo os seteres da vida....kkkk
+	public function login($Login, $password){
+
+		$sql = new Sql();
+		// A condição do Where esta declarado por referencia ":ID" a qual  será relacionada no array ao lado
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$Login, ":PASSWORD"=>$password));
+
+		if (count($results) > 0){
+
+			$row = $results[0];
+						
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}
+		 else {
+			// Exceção é a classe base para todas as exceções de usuário
+			// a classe exceção abaixo não funcionou
+			throw new Exception("Login e/ou senha inválidos.");
+			// echo "Login e/ou senha inválidos.";
+		}
+	}
 // O metódo abaixo __toString é chamado de metódo mágico. Quando da-se um echo "no caso aqui o echo e realizado pelo json_enconde" no objeto, ao inves de ser mostrado a estrutura do objeto será executado o que estiver dentro deste metódo __toString. 
+	
 	public function __toString(){
 
 		return json_encode(array(
@@ -70,7 +110,9 @@ class Usuario {
 			"deslogin"=>$this->getDeslogin(),
 			"dessenha"=>$this->getDessenha(),
 			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+			// "dtcadastro"=>$this->getDtcadastro()
 		));
 	}
 }
 ?>
+
--- sql.php
+++ sql.php
@@ -10,26 +10,43 @@ class Sql extends PDO {
 		$this->conn = new PDO("sqlsrv:Database=dbphp7;server=localhost\SQLEXPRESS;ConnectionPooling=0", "sa", "root");
 		
 		// abaixo é meu teste, na passagem do programa por aqui
-		echo "Passo 3.1 conecta ao banco de dados SQL". is_string($this->conn) . "<br>"."<br>";	
+		
+		$texto = "Passo 3.1 conecta ao banco de dados SQL". is_string($this->conn) . "<br>"."<br>";	
+		$msg = new Passei_por_aqui();
+		$msg->echoMsg($texto);
 	}
+	
 	
 	// Os scripts de apenas Select passam pela função abaixo, mas não alterão a variável statment, ou seja o script do select não ganha mais nenhuma instrução, pois o parametro $parameters estará vazio sem nenhuma condição ou into.
 	private function setParams($statment, $parameters = array()){
 		// abaixo é meu teste, na passagem do programa por aqui
-			echo "Passo 6.1 statment tem o cmd select ==> ". json_encode($statment) . "<br>";	
-			echo "Passo 6.2 parameters(array), mostrada com json ==> ". json_encode($parameters) . "<br>";	
-			echo "Passo 6.3 parameters(array), mostrada como string  ==> ". is_string($parameters) . "<br>"."<br>";	
+			$texto = "Passo 6.1 statment tem o cmd select ==> ". json_encode($statment) . "<br>";	
+			$msg = new Passei_por_aqui();
+			$msg->echoMsg($texto);
+
+			$texto = "Passo 6.2 parameters(array), mostrada com json ==> ". json_encode($parameters) . "<br>";	
+			$msg->echoMsg($texto);
+			
+			$texto = "Passo 6.3 parameters(array), mostrada como string  ==> ". is_string($parameters) . "<br>"."<br>";
+			$msg->echoMsg($texto);
+				
 		foreach ($parameters as $key => $value) {
 
 			//  a linha abaixo esta montado o meu script do sql como por exemplo: (":PASSWORD", $password)
 						
 			// abaixo é meu teste, na passagem do programa por aqui
-			echo "Passo 6.4 Monta a linha número : ". $n ." do SQL". $key . "<br>";	
-			echo "Passo 6.5 Monta a linha número : ". $n ." do SQL". $value . "<br>";	
+			$texto = "Passo 6.4 Monta a linha número : do SQL". $key . "<br>";
+			$msg->echoMsg($texto);
+
+			$texto = "Passo 6.5 Monta a linha número : do SQL". $value . "<br>";
+			$msg->echoMsg($texto);
+
 			// o $this abaixo é o mesmo que statment que é o metódo que foi criado
-			$this->setParam($key, $value);	
-			echo "Passo 6.9 statment : ". $this . "<br>"."<br>";	
-		
+			$this->setParam($statment, $key, $value);	
+			
+			$texto = "Passo 6.9 statment : ". is_string($this) . "<br>"."<br>";
+			$msg->echoMsg($texto);
+			
 		}
 
 	}
@@ -40,10 +57,18 @@ class Sql extends PDO {
 		
 		// abaixo esta sendo criado um metódo que se chama "$statment"
 		$statment->bindParam($key, $value);
-		echo "Passo 6.6 Monta a linha número : ". $n ." do SQL". $key . "<br>";	
-		echo "Passo 6.7 Monta a linha número : ". $n ." do SQL". $value . "<br>";	
-		echo "Passo 6.8 Monta a linha número : ". $n ." do SQL". $statment . "<br>";	
+		
+		$texto = "Passo 6.6 Monta a linha número : do SQL". $key . "<br>";	
+		$msg = new Passei_por_aqui();
+		$msg->echoMsg($texto);
 
+		$texto = "Passo 6.7 Monta a linha número : do SQL". $value . "<br>";	
+		$msg->echoMsg($texto);
+
+
+		$texto = "Passo 6.8 Monta a linha número : SQL". is_string($statment) . "<br>";	
+		$msg->echoMsg($texto);
+		
 		// abaixo é meu teste, na passagem do programa por aqui
 		
 
@@ -55,27 +80,43 @@ class Sql extends PDO {
 	public function query($rowQuery, $params = array()){
 		
 		// abaixo é meu teste, na passagem do programa por aqui
-		echo "Passo 5.1 Função query - Recebe o parametro rowQuery para completar com dados do script, mas no caso do select apenas não há mais dados a complementar ==> ".$rowQuery."<br>";	
-		echo "Passo 5.2 Função query - Recebe o parametro params que é um array, para completar com dados do script, mas no caso do select apenas, não há mais dados a complementar. Portanto como vemos, o segundo parametro esta vazio, pois a função select que chama esta passou o um array vazio ==> " . is_string($params)."<br>";
+		$texto = "Passo 5.1 Função query - Recebe o parametro rowQuery para completar com dados do script, mas no caso do select apenas não há mais dados a complementar ==> ".$rowQuery."<br>";	
+		$msg = new Passei_por_aqui();
+		$msg->echoMsg($texto);
+		
+		$texto = "Passo 5.2 Função query - Recebe o parametro params que é um array, para completar com dados do script, mas no caso do select apenas, não há mais dados a complementar. Portanto como vemos, o segundo parametro esta vazio, pois a função select que chama esta passou o um array vazio ==> " . is_string($params)."<br>";
+		$msg->echoMsg($texto);
+		
 		// como esta classe herda da classe PDO que já tem o cmd prepare, lá no PDO este cmd prepare já esta definido.
 		// $RawQuery é todo o meu cmd sql que será executado.
 		
 		// abaixo é meu teste, na passagem do programa por aqui
-		echo ("Passo 5.3 Prepara o comando 'prepare()' para afetar o Banco de Dados com a query a seguir ==> " . $rowQuery . "<br>");
-
+		$texto = "Passo 5.3 Prepara o comando 'prepare()' para afetar o Banco de Dados com a query a seguir ==> " . $rowQuery . "<br>";
+		$msg->echoMsg($texto);
+		
 		$stmt = $this->conn->prepare($rowQuery);
 		
 		// abaixo é meu teste, na passagem do programa por aqui
-		echo ("Passo 5.4 Mostra o valor da variável stmt ==> " );
-		var_dump($stmt)."<br>";
-		echo "<br>". ("Passo 5.5 Mostra o valor da variável params, como já dizemos acima esta vazia ==> " . is_string($params) . "<br>"."<br>");
+		$texto = "Passo 5.4 Mostra o valor da variável stmt ==> <br>";
+		$msg->echoMsg($texto);
+		
+		$texto = is_string($stmt) . "<br>";
+		$msg->echoMsg($texto);
+		
+		$texto = "<br>.Passo 5.5 Mostra o valor da variável params, como já dizemos acima esta vazia ==> " . is_string($params) . "<br>"."<br>";
+		$msg->echoMsg($texto);
 
 		$this->setParams($stmt, $params);
 		
 		$stmt->execute();
 		// abaixo é meu teste, na passagem do programa por aqui
-		echo "Passo 7.1 Afetou o Banco de Dados: ";
-		var_dump($stmt)."<br>";
+		
+		$texto = "Passo 7.1 Afetou o Banco de Dados ==> ";
+		$msg->echoMsg($texto);
+
+		$texto = is_string($stmt);
+		$msg->echoMsg($texto);
+
 
 		return $stmt;
 	}
@@ -87,10 +128,16 @@ class Sql extends PDO {
 		$stmt = $this->query($rowQuery, $params);
 		
 		// abaixo é meu teste, na passagem do programa por aqui
-		echo "<br>"."<br>"."Passo 8.1 Chama a função query para montar o sql e passa dois parametros : " . json_encode($stmt) . "<br>";	
-		echo "Passo 8.2 Chama a função query para montar o sql e passa dois parametros : " . json_encode($params) . "<br>";	
-		echo "Passo 8.3 Chama a função query para montar o sql e passa dois parametros : ". $rowQuery . " e " . "<br>" . is_string($this) . "<br>";	
+		$texto = "<br>"."<br>"."Passo 8.1 Chama a função query para montar o sql e passa dois parametros : " . json_encode($stmt) . "<br>";	
+		$msg = new Passei_por_aqui();
+		$msg->echoMsg($texto);
 		
+		$texto = "Passo 8.2 Chama a função query para montar o sql e passa dois parametros : " . json_encode($params) . "<br>";	
+		$msg->echoMsg($texto);
+
+		$texto = "Passo 8.3 Chama a função query para montar o sql e passa dois parametros : ". $rowQuery . " e " . "<br>" . is_string($this) . "<br>";	
+		$msg->echoMsg($texto);		
+			
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
